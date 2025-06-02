5fab382a1659eb0463691bff476d642413060e44
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-06-02 09:48:15 +0700

    Remove README.md and vite.config.js, update index.html to use main.ts, and enhance type support in Vue components. Added new dependencies and scripts for TypeScript support and build process.

--- SMS/vite.config.ts
+++ SMS/vite.config.ts
@@ -6,14 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
-    css: {
-        postcss: './postcss.config.cjs',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src',
-                import.meta.url)),
-        },
-    },
+  },
 })
